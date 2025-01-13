1a91b1b80a798e083a56a8e57b38f7d4ef9c10c9
Kai Ohno <Kaitaroohno@outlook.com>
2025-01-12 20:52:01 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TileAnimation from './components/TileAnimation';
+
+function App() {
+  return (
+    <div className="App">
+      <TileAnimation />
+    </div>
+  );
+}
+
+export default App;
